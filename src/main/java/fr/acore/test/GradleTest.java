81c24b5d824bafe5d2b18c5f7d4a26dcb9ec46d2
azefgh456 <antonin.lesaout@sfr.fr>
2021-06-10 16:07:57 +0200

    premier test avec gradle

---
+++ src/main/java/fr/acore/test/GradleTest.java
@@ -0,0 +1,11 @@
+package fr.acore.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GradleTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Je suis enabled j'ai tout bz");
+    }
+}
